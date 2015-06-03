16dbc4f81cc242fdd7ed14a1f516906456bb3bdf
sam3455 <www.vibha.com@gmail.com>
2015-06-03 20:46:41 +0530

    Update Paradox.java

--- Paradox.java
+++ Paradox.java
@@ -40,9 +40,12 @@ Random r=new Random();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("According to the birthday paradox in 23 peoples there is 50% chances that there is one pair such that "
-				+ "they have same birthdate and in 70 peoples there is 99.9% probabilty. for 1000 random experiments the percentage comes to be following"
-				+ ". these values are approximately coming near to the above values.");
+				System.out.println("According to the birthday paradox in 23 peoples"
+				+ "\n there is 50% chances that there is one pair such that "
+				+ "they have same birthdate and in 70 peoples there is 99.9% probabilty.\n"
+				+ " for 1000 random experiments the percentage comes to be following.\n"
+				+ " these values are approximately coming near to the above values.");
+
 new Paradox(23,1000);
 new Paradox(70,1000);
 	}
